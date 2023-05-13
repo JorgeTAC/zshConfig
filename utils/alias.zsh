54e4fbf4aac828e27f452a37b80a646a8b01e4ca
Jorge Aburto <jaburto@maimag.cl>
2023-05-12 22:53:21 -0400

    Add new functions and aliases 🥕

---
+++ utils/alias.zsh
@@ -0,0 +1,21 @@
+alias sysd="nvm use 10 && npm run ionic:build && nvm use 14.21.1 && firebase deploy --only hosting && nvm use node"
+alias zshe="code ~/.zsh"
+alias zshr="source ~/.zshrc && clear" 
+alias ionAndroid="ionic build --prod --release && npx cap sync android && npx cap copy android && npx cap open android"
+alias ionIos="ionic build --prod --release && npx cap sync ios && npx cap copy ios && npx cap open ios"
+
+# Javascript command line tools
+alias nvmpls="node ~/Workspace/Learning/nodejs/ng-manager/src/index.js"
+alias vsc="node ~/Workspace/Learning/nodejs/workspace-manager/index.js"
+
+# Git aliases
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit -m"
+alias gpl="git pull"
+alias gplf="git pull --force"
+alias gp="git push"
+alias gpf="git push --force"
+alias gpo="git push origin"
+
+
---
+++ utils/functions.zsh
@@ -0,0 +1,162 @@
+Cyan='\033[0;36m'
+NC='\033[0m' # No Color
+Green='\033[0;32m'
+# Create a help function to list all the aliases
+function ayuda () {
+  echo "${Green}  List of aliases:  ${NC}"
+  echo "${Green}------------------------${NC}"
+  echo "${Cyan} zshe ${NC}: edit zshrc file"
+  echo "${Cyan} zshr ${NC}: reload zshrc file"
+  echo "${Cyan} fbd ${NC}: deploy firebase hosting"
+  echo "${Cyan} ionAndroid ${NC}: build and open android project"
+  echo "${Cyan} ionIos ${NC}: build and open ios project"
+  echo "${Cyan} nvmpls ${NC}: run nvmpls"
+  echo "${Cyan} vsc ${NC}: run vsc"
+  echo "${Cyan} tscw ${NC}: run tsc --watch"
+  echo "${Cyan} firmFFM ${NC}: firmar apk"
+  echo "${Cyan} zipFFM ${NC}: zip apk"
+  echo "${Cyan} uploadTrans ${NC}: upload translations"
+  echo "${Cyan} fbs ${NC}: firebase serve"
+  echo "${Cyan} fbe ${NC}: firebase emulators:start"  
+  echo "${Cyan} ngs ${NC}: ng serve"
+  echo "${Cyan} ngb ${NC}: ng build"
+  echo "${Cyan} ion ${NC}: ionic serve"
+  echo "${Green}------------------------${NC}"
+  return
+}
+
+
+# function to execute the command node_modules/.bin/tsc --watch and detect if the folder is functions
+function tscw () {
+  # check if actual folder is named functions
+  if [ -d "functions" ]; then
+    cd functions
+  fi
+  node_modules/.bin/tsc --watch
+  return
+}
+
+firmFFM() {
+  jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ffmarket.jks -storepass 'ffm4rk3t' -keypass 'ffm4rk3t' android/app/build/outputs/bundle/release/app-release.aab ffmarket
+  return
+}
+
+zipFFM() {
+  ./zipalign -v -p 4 android/app/build/outputs/bundle/release/app-release.aab FFMARKET.aab
+  return
+}
+
+uploadTrans() {
+  cd ~/Workspace/FFMarket/ffmarket_translation && gsutil cp *.json gs://fitfood-22c23.appspot.com/market && git add . && git commit -m "[bot] Upload translations" && git push origin
+  return
+}
+
+function fbs () {
+ while [[ "$#" -gt 0 ]]
+do case $1 in
+    -p|--port) port="$2"
+    shift;;
+esac
+shift
+done
+# check if actual folder is named functions
+if [ -d "functions" ]; then
+  cd functions
+fi
+
+ nvm use 14.21.1 && firebase serve --only functions --port $port
+}
+
+fbe() {
+  if [[ $1 =~ ^(-o|--only) ]]
+  then
+      only="$2"
+  fi
+
+  if [  $# -eq 0 ]
+  then
+    only="0"
+  fi
+
+  if [ only -eq 0 ]
+  then 
+    nvm use 14.21.1 && firebase emulators:start
+    return
+  fi
+
+  nvm use 14.21.1 && firebase emulators:start --only $only
+
+}
+
+ngs() {
+  if [ $# -eq 0 ]
+  then
+    echo "Usage: ngs <version> <enviroment>"
+    ng serve -o
+    return
+  fi
+  if [ $# -lt 1 ]
+  then
+    nvm use $1 && ng serve -o
+    return
+  fi
+  if [ $# -lt 2 ]
+  then
+    nvm use $1 && ng serve -o --configuration=$2
+    return
+  fi
+  nvm use $1 && ng serve -o --configuration=$2 --port=$3
+
+  return
+}
+
+ngb() {
+  if [ $# -eq 0 ]
+  then
+    echo "Usage: ngs <version> <enviroment>"
+    return
+  fi
+  if [ $# -lt 2 ]
+  then
+    nvm use $1 && ng build --prod && nvm use 14.21.1 && firebase deploy --only hosting
+    return
+  fi
+
+
+  return
+} 
+
+ions() {
+  if [ $# -lt 0 ]
+  then
+    echo "Usage: ions <version> <enviroment>"
+    ionic serve -o
+    return
+  fi
+
+  if [ $# -lt 1 ]
+  then
+    nvm use $1 && ionic serve -o
+    return
+  fi
+  if [ $# -lt 2 ]
+  then
+    nvm use $1 && ionic serve -o --configuration=$2
+    return
+  fi
+  nvm use $1 && ionic serve -o --configuration=$2 --port=$3
+  return
+}
+
+function npmi() {
+  if [ $# -eq 0 ]
+  then
+    echo "Usage: npmi <package> "
+    npm install
+    return
+  fi
+  if [ $# -lt 1 ]
+  thennpm install $2 $3
+
+  return
+}
