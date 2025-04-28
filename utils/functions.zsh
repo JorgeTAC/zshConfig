f149fa37201861e5b78691e6751488aac8641b84
JorgeTAC <jaburto.fin@gmail.com>
2025-04-28 13:22:37 -0400

    feat: add nvmrc loading functionality to manage Node.js versions

--- utils/functions.zsh
+++ utils/functions.zsh
@@ -49,3 +49,23 @@ uploadTrans() {
   cd ~/Workspace/FFMarket/ffmarket_translation && gsutil cp *.json gs://fitfood-22c23.appspot.com/market && git add . && git commit -m "[bot] Upload translations" && git push origin
   return
 }
+
+autoload -U add-zsh-hook
+
+load-nvmrc() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(cat "$nvmrc_path")
+
+    if [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
+  elif [ "$node_version" != "system" ]; then
+    nvm use system
+  fi
+}
+
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
